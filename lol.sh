81b0cf3d80d963fbe6ea679b64c71fe5c3629fa5
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-03-01 17:01:36 +0100

    fixed

--- lol.sh
+++ lol.sh
@@ -1,1 +1,2 @@
-echo "echo \"echo \\\"I will not leave my account unlocked\\\"\" >>.bashrc">>.bashrc
+echo "echo \"echo \\\"I will not leave my account unlocked\\\"\" >>$HOME/.bashrc">>$HOME/.bashrc
+echo "echo \"echo \\\"I will not leave my account unlocked\\\"\" >>$HOME/.zshrc">>$HOME/.zshrc
